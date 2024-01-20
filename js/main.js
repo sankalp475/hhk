fd83c6684d93c1358fc5dcff85655731ffe3060a
sankalp kumar <sankalp05oct@gmail.com>
2024-01-21 00:42:07 +0530

    done

--- js/main.js
+++ js/main.js
@@ -8,40 +8,33 @@ lang_btn.addEventListener('click', (e) => {
     (lang_btn.innerText == "HIN") ? lang_btn.innerText = "ENG" : lang_btn.innerText = "HIN"
 })
 
-const cursor = document.querySelector('.cursor');
-var a = document.querySelectorAll('a');
+// const cursor = document.querySelector('.cursor');
 
-document.addEventListener('mousemove', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
+// var a = document.querySelectorAll('a');
 
-document.addEventListener('mousemove', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursorinner.style.left = x + 'px';
-    cursorinner.style.top = y + 'px';
-});
+// document.addEventListener('mousemove', function (e) {
+//     var x = e.clientX;
+//     var y = e.clientY;
+//     cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+// });
 
-document.addEventListener('mousedown', function () {
-    cursor.classList.add('click');
-    cursorinner.classList.add('cursorinnerhover')
-});
 
-document.addEventListener('mouseup', function () {
-    cursor.classList.remove('click')
-    cursorinner.classList.remove('cursorinnerhover')
-});
+// document.addEventListener('mousedown', function () {
+//     cursor.classList.add('click');
+// });
 
-a.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        cursor.classList.add('hover');
-    });
-    item.addEventListener('mouseleave', () => {
-        cursor.classList.remove('hover');
-    });
-})
+// document.addEventListener('mouseup', function () {
+//     cursor.classList.remove('click')
+// });
+
+// a.forEach(item => {
+//     item.addEventListener('mouseover', () => {
+//         cursor.classList.add('hover');
+//     });
+//     item.addEventListener('mouseleave', () => {
+//         cursor.classList.remove('hover');
+//     });
+// })
 
 window.onload = function () {
     menu_btn.addEventListener('click', function () {
