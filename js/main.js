3f6ee5ff98ea6fd2fb9d4531a796efaf1fe03e84
sankalp kumar <sankalp05oct@gmail.com>
2024-01-21 00:37:09 +0530

    done

--- js/main.js
+++ js/main.js
@@ -3,18 +3,45 @@ const contactForm = document.querySelector('.contact__form');
 const menu_btn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 const lang_btn = document.querySelector('.hindi--btn');
-lang_btn.addEventListener('click',(e)=>{
-   console.log(lang_btn.innerText);
-   (lang_btn.innerText == "HIN") ? lang_btn.innerText = "ENG" : lang_btn.innerText = "HIN"
+lang_btn.addEventListener('click', (e) => {
+    console.log(lang_btn.innerText);
+    (lang_btn.innerText == "HIN") ? lang_btn.innerText = "ENG" : lang_btn.innerText = "HIN"
 })
 
 const cursor = document.querySelector('.cursor');
+var a = document.querySelectorAll('a');
 
-document.addEventListener('mousemove', (dets) => {
-    cursor.style.left = (dets.x + -10) +  'px';
-    cursor.style.top = (dets.y + -10) + 'px';
-    cursor.style.zIndex = "1"
+document.addEventListener('mousemove', function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
+
+document.addEventListener('mousemove', function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    cursorinner.style.left = x + 'px';
+    cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function () {
+    cursor.classList.add('click');
+    cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function () {
+    cursor.classList.remove('click')
+    cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        cursor.classList.add('hover');
+    });
+    item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover');
+    });
+})
 
 window.onload = function () {
     menu_btn.addEventListener('click', function () {
